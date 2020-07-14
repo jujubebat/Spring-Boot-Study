f15951ed7d7693234f4fd9dba235805d014a3dc4
jujubebat <bbwwpark@naver.com>
2020-07-14 23:28:02 +0900

    '자동설정실습완료.

--- byungwookspringbootstarter/src/main/java/com/jujubebat/HolomanConfiguration.java
+++ byungwookspringbootstarter/src/main/java/com/jujubebat/HolomanConfiguration.java
@@ -1,5 +1,7 @@
 package com.jujubebat;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,13 +9,16 @@ import org.springframework.context.annotation.Configuration;
 // resources/META-INF/spring.factories에 이 설정 파일을 명시해주면
 // 스프링 부트 Enable auto Configuration이 이 설정 파일을 읽는다.
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HolomanConfiguration {
 
     @Bean
-    public Holoman holoman(){
+    @ConditionalOnMissingBean // Holoman 빈이 등록되지 않았을때만 빈을 등록하라는 뜻.
+    public Holoman holoman(HolomanProperties properties){
         Holoman holoman = new Holoman();
-        holoman.setHowLong(5);
-        holoman.setName("ByungWook");
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
         return holoman;
     }
+
 }
--- spring-boot-getting-started/src/main/java/com/jujubebat/Application.java
+++ spring-boot-getting-started/src/main/java/com/jujubebat/Application.java
@@ -3,6 +3,7 @@ package com.jujubebat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -14,17 +15,30 @@ public class Application {
         application.run(args);
     }
 
-    // 이렇게 직접 빈을 등록하면 무시된다.
-    // 1단계로 @ComponentScan을 통해 아래 빈이 등록된다.
-    // 2단계로 @EnableAutoConfiguration로 빈이 등록된다.
-    // byungwookspringbootstarter 프로젝트에서 등록해둔 빈이 등록되면서 아래 빈은 덮어쓰여진다.
-    @Bean
+    /*
+     이렇게 직접 빈을 등록하면 무시된다.
+     1단계로 @ComponentScan을 통해 아래 빈이 등록된다.
+     2단계로 @EnableAutoConfiguration로 빈이 등록된다.
+     byungwookspringbootstarter 프로젝트에서 등록해둔 빈이 @EnableAutoConfiguration 에의해
+     등록되면서 아래 빈은 덮어쓰여진다.
+     @ConditionalOnMissingBean을 추가하여 이미 빈이 존재하면 2단계 빈 등록 단계에서 빈을 등록되지 않도록 한다.
+    */
+
+    /* @Bean
     public Holoman holoman() {
         Holoman holoman = new Holoman();
         holoman.setName("byunwook");
         holoman.setHowLong(50);
         return holoman;
-    }
+    } */
+
+    /*
+     위와 같이 빈을 등록할 수 있지만,
+     application.properties에 holoman 필드 값을 명시하여 빈을 재정의할 필요없이 application.properties 조작만으로
+     빈을 등록할 수 있다.
+     (byungwookspringbootstarter 프로젝트의 HolomanConfiguration 클래스의 @EnableConfigurationProperties와
+     HolomanProperties 클래스를 참고하자)
+    */
 }
 
 /*
